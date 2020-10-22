d8764345cc488ea5d926f7f47f401bc4f56d62eb
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2020-10-22 18:40:09 -0500

    :sparkles: Added minimal project setup.

---
+++ src/pages/MovieDetail.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const MovieDetail = props => {
+  return (
+    <div>
+      Movie Detail - Todo lo relacionado a la pelicual que se selecciono
+    </div>
+  )
+}
+
+MovieDetail.propTypes = {
+
+}
+
+export default MovieDetail
